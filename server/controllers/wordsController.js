ff4449d9ffa7164003f012860457874713ea1492
adon <adnlwly@gmail.com>
2023-01-25 11:46:23 +0000

    Update randomWord JSON response

--- server/controllers/wordsController.js
+++ server/controllers/wordsController.js
@@ -3,12 +3,17 @@ const client = require("../database");
 const randomWord = async (request, response) => {
   try {
     const randomId = Math.floor(Math.random() * 2498 + 1);
+
     const sql = "SELECT word FROM words WHERE id = $1";
-    const randomWord = await client.query(sql, [randomId]);
+
+    const data = await client.query(sql, [randomId]);
+
+    const randomWord = data.rows[0].word;
 
     response.status(200).json({ word: randomWord });
   } catch (error) {
     console.log(error.message);
+
     response.status(400).json({ error: error.message });
   }
 };
