b72f6011fd7b7a4675bed2940d05043805920f34
adon <adnlwly@gmail.com>
2023-01-25 11:59:18 +0000

    Set up GET today's word route

--- server/routes/words.js
+++ server/routes/words.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { randomWord } = require("../controllers/wordsController");
+const { randomWord, todaysWord } = require("../controllers/wordsController");
 const router = express.Router();
 
 router.get("/random", randomWord);
+router.get("/today", todaysWord);
 
 module.exports = router;
