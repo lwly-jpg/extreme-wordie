82d6363c395a693019915ba5e77c3065d58bb2b6
adon <adnlwly@gmail.com>
2023-01-25 12:52:29 +0000

    Add random word fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
+import { useEffect, useState } from "react";
 import Nav from "./components/Nav/Nav";
 
 function App() {
+  const [randomWord, setRandomWord] = useState();
+
+  useEffect(() => {
+    fetch("https://extreme-wordie.onrender.com/words/random")
+      .then((response) => response.json())
+      .then((data) => setRandomWord(data.word));
+  }, []);
+
   return (
     <div className="App">
       <Nav />
