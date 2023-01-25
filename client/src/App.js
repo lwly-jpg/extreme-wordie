96d8a126503ec2e16e1a6c060ef4c8698d165709
adon <adnlwly@gmail.com>
2023-01-25 16:44:15 +0000

    Add Keyboard component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Nav from "./components/Nav/Nav";
 import Game from "./components/Game/Game";
+import Keyboard from "./components/Keyboard/Keyboard";
 
 function App() {
   const [randomWord, setRandomWord] = useState();
@@ -15,6 +16,7 @@ function App() {
     <div className="App">
       <Nav />
       <Game />
+      <Keyboard />
     </div>
   );
 }
