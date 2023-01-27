bc29d55b9d62dac1084683c703da785a090168e1
adon <adnlwly@gmail.com>
2023-01-27 18:45:07 +0000

    Style loss popup

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,19 +1,23 @@
 import style from "./Modal.module.css";
 
-const Modal = ({win}) => {
-
+const Modal = ({ win }) => {
   return (
     <div className={style.container}>
-      {win ?
-      <div className={style.popup}>
-        <h1 className={style.win}>You won!</h1>
-        <h2 className={style.word}>Word: XXX</h2>
-        <p className={style.text}>You got it in X / 5 attempts!</p>
-      </div> :
-      <div className={style.popup}>beter luck next time</div>
-      }
+      {win ? (
+        <div className={style.popup}>
+          <h1 className={style.win}>You won!</h1>
+          <h2 className={style.word}>Word: XXX</h2>
+          <p className={style.text}>You got it in X / 5 attempts!</p>
+        </div>
+      ) : (
+        <div className={style.popup}>
+          <h1 className={style.loss}>Next time!</h1>
+          <h2 className={style.word}>Word: XXX</h2>
+          <p className={style.text}>You used up all 5 attempts!</p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Modal;
