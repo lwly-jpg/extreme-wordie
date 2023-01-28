a328f47f2729d42d8e90aa9523ce99280488539c
adon <adnlwly@gmail.com>
2023-01-28 12:35:51 +0000

    Move Keyboard functions into custom hook

--- client/src/components/Keyboard/Keyboard.js
+++ client/src/components/Keyboard/Keyboard.js
@@ -1,83 +1,15 @@
 import styles from "./Keyboard.module.css";
 import { useContext } from "react";
 import { GameContext } from "../Game/Game";
+import useKeyboard from "../../hooks/useKeyboard";
 
 const Keyboard = () => {
-  const {
-    todaysWord,
-    currentAttempt,
-    round,
-    usedLetters,
-    setUsedLetters,
-    setCurrentAttempt,
-    setAttempts,
-    setRound,
-    setWin,
-  } = useContext(GameContext);
+  const { usedLetters } = useContext(GameContext);
+  const { handleKeyClick, handleBackspace, handleReturn } = useKeyboard();
 
   const firstRow = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"];
   const secondRow = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
   const thirdRow = ["z", "x", "c", "v", "b", "n", "m"];
-
-  const handleKeyClick = (letter) => {
-    if (currentAttempt.length === 6) {
-      return;
-    }
-    setCurrentAttempt((prev) => prev + letter);
-  };
-
-  const handleBackspace = (event) => {
-    event.preventDefault();
-
-    setCurrentAttempt((prev) => prev.slice(0, -1));
-  };
-
-  const handleReturn = (event) => {
-    event.preventDefault();
-
-    if (currentAttempt === todaysWord) {
-      setWin(true);
-    }
-
-    if (round > 4) {
-      console.log("Game is over");
-      return;
-    }
-
-    if (currentAttempt.length !== 6) {
-      return;
-    }
-
-    setAttempts((prevAttempts) => {
-      let newAttempts = [...prevAttempts];
-
-      const formattedAttempt = [...currentAttempt].map((letter, index) => {
-        if (letter === todaysWord[index]) {
-          setUsedLetters(prev => ({...prev, [letter]: "correct" }))
-          return { letter: letter, result: "correct" };
-        } else if ([...todaysWord].includes(letter)) {
-          if (usedLetters[letter] !== "correct") {
-            setUsedLetters(prev => ({...prev, [letter]: "almost" }))
-          }
-          return { letter: letter, result: "almost" };
-        } else {
-          if (usedLetters[letter] !== "correct" && usedLetters[letter] !== "almost") {
-            setUsedLetters(prev => ({...prev, [letter]: "incorrect" }))
-          }
-          return { letter: letter, result: "incorrect" };
-        }
-      });
-
-      newAttempts[round] = formattedAttempt;
-      return newAttempts;
-    });
-
-    setCurrentAttempt("");
-
-    setRound((prevRound) => {
-      return prevRound + 1;
-    });
-  };
 
   return (
     <div className={styles.keyboard}>
